262f43ae53f2a879ad1343e8f508fc580ec4f471
Владас Куодис <vlad250577@yandex.ru>
2023-08-08 08:52:21 +0300

    My first commit

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -9,9 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = False
+# DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(', ')
+
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(', ')
+ALLOWED_HOSTS = []
 
 AUTH_USER_MODEL = 'users.User'
 
