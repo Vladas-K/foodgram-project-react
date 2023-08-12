135c80ea43770da3911aa70f2df970e3bbf1f5a4
Владас Куодис <vlad250577@yandex.ru>
2023-08-12 13:30:22 +0300

    Move gunicorn

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -26,7 +26,8 @@ class RecipeFilter(FilterSet):
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
-        if value and self.request.user.is_authenticated:
+        user = self.request.user
+        if value and user.is_authenticated:
             return queryset.filter(shopping_list__user=self.request.user)
         return
 
