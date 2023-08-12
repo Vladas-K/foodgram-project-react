3510e16aca811d5f24b18de82147effe47ebe9b3
Владас Куодис <vlad250577@yandex.ru>
2023-08-12 12:16:04 +0300

    Fix counter

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -12,7 +12,8 @@ class RecipeFilter(FilterSet):
     )
 
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
-    is_in_shopping_cart = filters.BooleanFilter(method='filter_is_in_shopping_cart')
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='filter_is_in_shopping_cart')
 
     class Meta:
         model = Recipe
@@ -23,7 +24,7 @@ class RecipeFilter(FilterSet):
         if value and user.is_authenticated:
             return queryset.filter(in_favorite__user=user)
         return queryset
-    
+
     def filter_is_in_shopping_cart(self, queryset, name, value):
         if value and self.request.user.is_authenticated:
             return queryset.filter(shopping_list__user=self.request.user)
